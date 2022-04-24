8d7ae30d5b49ee00df183ecb01fe7168d5868096
Marco Eisenring <marco.eisenring [at] author-gmail.com>
2022-04-24 11:58:35 +0200

    Configure the minimum level for events passed through the sink

--- src/Serilog.Sinks.Spectre/SpectreConsoleSinkExtensions.cs
+++ src/Serilog.Sinks.Spectre/SpectreConsoleSinkExtensions.cs
@@ -1,6 +1,6 @@
-using System;
-using Serilog;
 using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace Serilog.Sinks.Spectre
 {
@@ -8,11 +8,24 @@ namespace Serilog.Sinks.Spectre
 	{
 		const string DefaultConsoleOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
 
+		/// <summary>
+		/// Write log events to the console using Spectre.Console.
+		/// </summary>
+		/// <param name="loggerConfiguration">Logger sink configuration.</param>
+		/// <param name="outputTemplate">A message template describing the format used to write to the sink.
+		/// The default is "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}".</param>
+		/// <param name="restrictedToMinimumLevel">The minimum level for
+		/// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+		/// <param name="levelSwitch">A switch allowing the pass-through minimum level
+		/// to be changed at runtime.</param>
+		/// <returns>Configuration object allowing method chaining.</returns>
 		public static LoggerConfiguration Spectre(
 			this LoggerSinkConfiguration loggerConfiguration,
-			string outputTemplate = DefaultConsoleOutputTemplate)
+			string outputTemplate = DefaultConsoleOutputTemplate,
+			LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+			LoggingLevelSwitch levelSwitch = null)
 		{
-			return loggerConfiguration.Sink(new SpectreConsoleSink(outputTemplate));
+			return loggerConfiguration.Sink(new SpectreConsoleSink(outputTemplate), restrictedToMinimumLevel, levelSwitch);
 		}
 	}
 }
